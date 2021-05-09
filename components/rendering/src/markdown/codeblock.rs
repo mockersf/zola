4c0ed7516dd101e139f0a09bd6fb0a837878c257
François Mockers <mockersf@gmail.com>
2021-05-09 23:08:01 +0200

    allow code block to hide lines

--- components/rendering/src/markdown/codeblock.rs
+++ components/rendering/src/markdown/codeblock.rs
@@ -17,6 +17,8 @@ pub struct CodeBlock<'config> {
 
     /// List of ranges of lines to highlight.
     highlight_lines: Vec<Range>,
+    /// List of ranges of lines to hide.
+    hidden_lines: Vec<Range>,
     /// The number of lines in the code block being processed.
     num_lines: usize,
 }
@@ -36,6 +38,7 @@ impl<'config> CodeBlock<'config> {
             theme,
 
             highlight_lines: fence_info.highlight_lines,
+            hidden_lines: fence_info.hidden_lines,
             num_lines: 0,
         }
     }
@@ -61,6 +64,9 @@ impl<'config> CodeBlock<'config> {
         let hl_lines = self.get_highlighted_lines();
         color_highlighted_lines(&mut highlighted, &hl_lines, hl_background);
 
+        let hidden_lines = self.get_hidden_lines();
+        let highlighted = hide_hidden_lines(highlighted, &hidden_lines);
+
         styled_line_to_highlighted_html(&highlighted, self.background)
     }
 
@@ -80,6 +86,17 @@ impl<'config> CodeBlock<'config> {
     fn get_highlighted_lines(&self) -> HashSet<usize> {
         let mut lines = HashSet::new();
         for range in &self.highlight_lines {
+            for line in range.from..=min(range.to, self.num_lines) {
+                // Ranges are one-indexed
+                lines.insert(line.saturating_sub(1));
+            }
+        }
+        lines
+    }
+
+    fn get_hidden_lines(&self) -> HashSet<usize> {
+        let mut lines = HashSet::new();
+        for range in &self.hidden_lines {
             for line in range.from..=min(range.to, self.num_lines) {
                 // Ranges are one-indexed
                 lines.insert(line.saturating_sub(1));
@@ -179,4 +196,30 @@ fn color_highlighted_lines(data: &mut [(Style, &str)], lines: &HashSet<usize>, b
             current_line += 1;
         }
     }
+}
+
+fn hide_hidden_lines<'a>(
+    data: Vec<(Style, &'a str)>,
+    lines: &HashSet<usize>,
+) -> Vec<(Style, &'a str)> {
+    if lines.is_empty() {
+        return data;
+    }
+
+    let mut current_line = 0;
+
+    let mut kept = Vec::new();
+
+    for item in data {
+        if !lines.contains(&current_line) {
+            kept.push(item);
+        }
+
+        // We split the lines such that every newline is at the end of an item.
+        if item.1.ends_with('\n') {
+            current_line += 1;
+        }
+    }
+
+    kept
 }
